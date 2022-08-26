ddfe09368ec99e2ed6f803c657d33f0045485e48
Tobeworks <hallo@tobeworks.de>
2022-08-26 12:49:33 +0200

    init

---
+++ src/components/landing-page.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Landingpage</h1>
+    </div>
+</template>
+
+<script>
+import '../scss/style.scss'
+import '../scss/animate.scss'
+
+export default {}
+</script>
+
+<style lang="scss">
+
+</style>
