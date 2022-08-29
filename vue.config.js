b085d863b4bda7691a1f0a5e332ad0881a0106a6
Tobeworks <hallo@tobeworks.de>
2022-08-29 11:30:36 +0200

    image opti

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,47 @@
 const { defineConfig } = require('@vue/cli-service')
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+const { extendDefaultPlugins } = require("svgo");
+
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: './',
   productionSourceMap: process.env.NODE_ENV != 'production',
+  configureWebpack: {
+    optimization: {
+      minimizer: [
+        new ImageMinimizerPlugin({
+          minimizer: {
+            implementation: ImageMinimizerPlugin.imageminMinify,
+            options: {
+              // Lossless optimization with custom option
+              // Feel free to experiment with options for better result for you
+              plugins: [
+                ["gifsicle", { interlaced: true }],
+                ["jpegtran", { progressive: true }],
+                ["optipng", { optimizationLevel: 5 }],
+                // Svgo configuration here https://github.com/svg/svgo#configuration
+                [
+                  "svgo",
+                  {
+                    plugins: extendDefaultPlugins([
+                      {
+                        name: "removeViewBox",
+                        active: false,
+                      },
+                      {
+                        name: "addAttributesToSVGElement",
+                        params: {
+                          attributes: [{ xmlns: "http://www.w3.org/2000/svg" }],
+                        },
+                      },
+                    ]),
+                  },
+                ],
+              ],
+            },
+          },
+        }),
+      ],
+    }
+  },
 })
