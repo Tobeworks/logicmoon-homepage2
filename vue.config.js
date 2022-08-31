6aebdcbdea9ba28c0fcfafefddb2e519465a9ec7
Tobeworks <hallo@tobeworks.de>
2022-08-31 18:15:19 +0200

    image compressor working

--- vue.config.js
+++ vue.config.js
@@ -17,65 +17,26 @@ module.exports = defineConfig({
       minimizer: [
         new ImageMinimizerPlugin({
           minimizer: {
-            implementation: ImageMinimizerPlugin.imageminMinify,
+            implementation: ImageMinimizerPlugin.squooshMinify,
             options: {
-              // Lossless optimization with custom option
-              // Feel free to experiment with options for better result for you
-              plugins: [
-                ["gifsicle", { interlaced: true }],
-                ["jpegtran", { progressive: true }],
-                ["optipng", { optimizationLevel: 5 }],
-                // Svgo configuration here https://github.com/svg/svgo#configuration
-                [
-                  "svgo",
-                  {
-                    plugins: extendDefaultPlugins([
-                      {
-                        name: "removeViewBox",
-                        active: false,
-                      },
-                      {
-                        name: "addAttributesToSVGElement",
-                        params: {
-                          attributes: [{ xmlns: "http://www.w3.org/2000/svg" }],
-                        },
-                      },
-                    ]),
-                  },
-                ],
-              ],
-            },
-          },
-          generator: [
-            {
-              // You can apply generator using `?as=webp-100-50`, you can use any name and provide more options
-              preset: "jpeg-100-100",
-              // implementation: ImageMinimizerPlugin.sharpGenerate,
-              implementation: ImageMinimizerPlugin.squooshGenerate,
-              options: {
-                resize: {
-                  enabled: true,
-                  width: 500,
-                  height: 500,
+              encodeOptions: {
+                mozjpeg: {
+                  // That setting might be close to lossless, but it’s not guaranteed
+                  // https://github.com/GoogleChromeLabs/squoosh/issues/85
+                  quality: 60,
                 },
-                encodeOptions: {
-                  webp:{
-                    quality: 60,
-                  },
-                  mozjpeg: {
-                    quality: 60,
-                    resize: {
-                      enabled: true,
-                      width: 500,
-                      height: 500,
-                    }
-                  },
+                webp: {
+                  lossless: 1,
+                },
+                avif: {
+                  // https://github.com/GoogleChromeLabs/squoosh/blob/dev/codecs/avif/enc/README.md
+                  cqLevel: 0,
                 },
               },
             },
-          ],
+          },
         }),
       ],
-    }
+    },
   },
 })
